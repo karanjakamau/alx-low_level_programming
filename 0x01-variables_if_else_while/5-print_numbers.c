b46fa1d6cb301a52fc2b0889bc15afe3ef0c0883
karanjakamau <kamaujames2@gmail.com>
2021-06-21 17:35:19 +0300

    digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stio.h>
+#include <stlib.h>
+
+int main(void)
+{
+    int i;
+    for ( i = 0; i < 10; i++)
+    {
+        putchar(i);
+        putchar('\n');
+    }
+    
+    return(0);
+}
