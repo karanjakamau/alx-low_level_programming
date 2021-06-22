f58b4370c8bf5bba305126099fa616feb638bdeb
karanjakamau <kamaujames2@gmail.com>
2021-06-22 15:47:07 +0300

    loop

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -2,7 +2,12 @@
 
 int main (void)
 {
-char c = "H";    
-_putchar(c);
+char name[9] = {'H','o','l','b','e','r','t','o','n'};    
+int c;
+for ( c = 0; c < 9; c++)
+{
+    _putchar(name[c]);
+}
+_putchar('\n');
 return (0);
 }
