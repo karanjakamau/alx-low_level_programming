0b80b32609feddbe9b720a4573dfbb9d11756e48
karanjakamau <kamaujames2@gmail.com>
2021-06-22 22:05:53 +0300

    correction

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,10 +1,30 @@
 #include "holberton.h"
 /**
- * main - prints the name holberton
+ * jack_bauer - prints the time
  * using putchar function
- * Return: the function returns 0
+ * Return: the function returns void
  */
 void jack_bauer(void)
 {
-    
+int hours = 0;
+int minutes = 0;
+int hours_remainder;
+int mins_remainder;
+while (hours <= 23)
+{
+while (minutes <= 59)
+{
+mins_remainder = minutes % 10;
+hours_remainder = hours % 10;
+_putchar(hours / 10 + '0');
+_putchar(hours_remainder + '0');
+_putchar(':');
+_putchar(minutes / 10 + '0');
+_putchar(mins_remainder + '0');
+minutes++;
+_putchar('\n');
+}
+hours++;
+minutes = 0;
+}
 }
