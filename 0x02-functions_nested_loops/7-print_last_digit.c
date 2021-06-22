d6d3a1080b364c154c88728da66816bedfad1b64
karanjakamau <kamaujames2@gmail.com>
2021-06-22 20:01:45 +0300

    digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,14 +6,14 @@ int print_last_digit(int n)
     if (n >= 0)
     {
      x = n % 10;
-     _putchar(n + '0');
+     _putchar(x + '0');
      return (x);   
     }
     else
     {
         n *= -1;
         x = n % 10;
-        _putchar(n + '0');
+        _putchar(x + '0');
         return (x);
     }
     
