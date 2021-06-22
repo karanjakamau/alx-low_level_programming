b97da96c4a1895ab093be7e62fad79fa9e81eae3
karanjakamau <kamaujames2@gmail.com>
2021-06-22 16:18:25 +0300

    z

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,7 +6,7 @@ void print_alphabet_x10(void)
     for ( i = 0; i < 10; i++)
     {
         char alpha;
-        for ( alpha = 'a'; alpha < 'z'; alpha++)
+        for ( alpha = 'a'; alpha <= 'z'; alpha++)
         {
             _putchar(alpha);
         }
