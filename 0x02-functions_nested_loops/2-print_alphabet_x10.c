423b5a3fa2917d42ab428f820731974c16c5b2d3
karanjakamau <kamaujames2@gmail.com>
2021-06-22 16:13:46 +0300

    tentimes

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+void print_alphabet_x10(void)
+{
+    int i;
+    for ( i = 0; i < 10; i++)
+    {
+        char alpha;
+        for ( alpha = 'a'; alpha < 'z'; alpha++)
+        {
+            _putchar(alpha);
+        }
+        
+    }
+    _putchar('\n');
+    return (0);
+}
