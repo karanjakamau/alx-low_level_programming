793de1120fa048ef62b387a5dcb9d912d5d3f14c
karanjakamau <kamaujames2@gmail.com>
2021-06-24 11:42:57 +0300

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _isupper - Checks for uppercase characters
+ * @c: the character to check
+ * 
+ * Return: 1 if true, 0 is false.
+ */
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
