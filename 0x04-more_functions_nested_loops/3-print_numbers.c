d05136f455c641c004a930af10210dfef7242a73
karanjakamau <kamaujames2@gmail.com>
2021-06-24 12:37:44 +0300

    print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * print_numbers - print numbers 0 - 9
+ * Return: returns void
+ */
+
+void print_numbers(void)
+{
+int i = 0;
+while (i <= 9)
+{
+_putchar(i + '0');
+i++;
+}   
+}
