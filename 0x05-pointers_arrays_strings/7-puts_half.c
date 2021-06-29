e2e81025a917a594f2e48f4765d0e82587748c5b
karanjakamau <kamaujames2@gmail.com>
2021-06-29 22:45:32 +0300

    puthalf

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <string.h>
+/**
+ * puts_half - print second half of the string
+ * @str: string pointer
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+int len = strlen(str);
+int i;
+
+for (i = (len / 2); i < len; i++)
+{
+
+if ((len - 1) % 2 == 0)
+
+{
+_putchar(*(str + i));
+}
+else if ((len - 1) % 2 != 0)
+{
+_putchar(*(str + i));
+}
+}
+}
