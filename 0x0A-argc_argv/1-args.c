c6c0b22ebf851d5c4e951f6dcb8b684d02d4323b
karanjakamau <kamaujames2@gmail.com>
2021-07-12 22:36:54 +0300

    nargs

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - print number of arguments
+ * @argc: count
+ * @argv: arguments
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+(void) argv;
+printf("%d\n", argc);
+return (0);
+}
